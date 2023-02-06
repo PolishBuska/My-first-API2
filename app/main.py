f93fba9a4b114324e610a2bb80ce0607c272d476
PolishBuska <375292765815@mail.ru>
2023-02-06 15:03:33 +0300

    add dockerfile and docker-compose

--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
-from fastapi import FastAPI,Depends
-from . import models
-from .database import engine,get_db
-from .routs import post,users,auth,vote
+from fastapi import FastAPI
+from .routs import post, users, auth, vote
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session
-origins =["*"]
 
-#models.Base.metadata.create_all(bind=engine)
+origins = ["*"]
+
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Box social media"
@@ -14,13 +12,17 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials = True,
+    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers= ["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(post.router)
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+@app.get("/hello")
+def root():
+    return {"Message":"Goood"}
 
