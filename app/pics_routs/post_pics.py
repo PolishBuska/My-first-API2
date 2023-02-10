7b5ff698d559c9aa6a092f650c4c55333a2401a5
PolishBuska <375292765815@mail.ru>
2023-02-11 00:20:34 +0300

    created upload file system

--- app/pics_routs/post_pics.py
+++ app/pics_routs/post_pics.py
@@ -1,4 +1,4 @@
-from fastapi import status,HTTPException,Response,FastAPI,Depends,APIRouter
+from fastapi import status,HTTPException,Response,FastAPI,Depends,APIRouter,UploadFile,File
 from .. import models,schemas,JWT_SERVICE
 from sqlalchemy.orm import Session
 from ..database import get_db
@@ -6,6 +6,7 @@ from typing import List,Optional
 from sqlalchemy import func
 from fastapi.staticfiles import StaticFiles
 import os
+import shutil
 
 router = APIRouter(
     prefix='/jpgs',
@@ -20,6 +21,14 @@ def images():
         out.append({"name":filename.split('.')[0],
                     "path":"/static/images/" + filename})
     return out
+
+@router.post('/')
+async def create_file(file: UploadFile = File(...)):
+    file_location = f"app/static/images/{file.filename}"
+    with open(file_location, "wb+") as file_object:
+        file_object.write(file.file.read())
+    return {"info": f"file '{file.filename}' saved at '{file_location}'"}
+
 
 
 
